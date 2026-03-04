d3ef7202ed8575575520847f398cbbb560cb9e97
Pedro Augusto <pedroaugusto10e@gmail.com>
2026-03-04 13:50:55 -0300

    Update tbl_clientes.sql

--- Banco de Dados Clientes/tbl_clientes.sql
+++ Banco de Dados Clientes/tbl_clientes.sql
@@ -10,6 +10,6 @@ data_nascimento DATE NOT NULL
 
 )
 
-INSERT INTO clientes (nome, cpf, telefone, email, endereço, data_de_nascimento)
+INSERT INTO clientes (nome, cpf, telefone, email, endereço, data_nascimento)
 VALUES ('Pedro Augusto dos Santos Mendonça', '54432334334', '11943743445', 'pedroaugusto10e@gmail.com', 'Rua Agostinho dos Santos, Parque Mirante da Mata', '15/04/2008')
 
